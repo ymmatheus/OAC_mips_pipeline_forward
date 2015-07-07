b5756a3699dd14f5168dab135c0c64924598af75
DouglasShiro <douglasshiro@hotmail.com>
2015-07-07 14:20:02 -0300

    Adição arquivos
    mipsPipe_line, top level entity
    controleMIPS, unidade de controle

---
+++ controleMIPS.vhd
@@ -0,0 +1,10 @@
+-- Unidade de Controle MIPS
+-- 					Sinais de Controle
+-- Execução/ Calculo endereço: RegDst, OpALU1, opALU0, OrigALU
+-- Acesso a Memoria: Branch, LeMem, EscreveMem 
+-- Escrita do Resultado: EscreveReg, MemtoReg
+ 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
